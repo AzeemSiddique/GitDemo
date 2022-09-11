ca249831946a361f9d7b8a43099b7bd8a2d1bf80
Muhammad Noman Ilyas <Muhammad Noman Ilyas>
2022-09-11 16:15:52 +0500

    Calc.py Created & Updated with addition function Author:MNI

---
+++ calc.py
@@ -0,0 +1,13 @@
+def Addition(a , b):
+    sum=int(a)+int(b)
+    return sum
+
+
+
+num1=input("Enter 1st number: ")
+
+
+num2=input("Enter 2nd number: ")
+
+print("Sum of " , num1 , "&" , num2 , "is" , Addition(num1 , num2))
+
